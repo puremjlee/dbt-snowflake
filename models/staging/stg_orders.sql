5c58d28f9c6073ddcc2def10dae994d6b7815a3e
puremjlee@gmail.com <puremjlee@gmail.com>
2022-09-24 11:46:20 +0000

    developed report by customer and product

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -8,10 +8,12 @@ o.ordersellingprice - o.ordercostprice as orderprofit,
 o.ordersellingprice,
 o.ordercostprice,
 --from raw customer
+c.customerid,
 c.customername,
 c.segment,
 c.country,
 --from raw product
+p.productid,
 p.category,
 p.productname,
 p.subcategory
